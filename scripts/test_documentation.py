d12a0ccb6839b17b36956684cc2712f4c07dd072
Fabian Pijcke <fabian.pijcke@gmail.com>
2025-04-30 15:53:23 +0200

    Command-line arguments.

--- scripts/test_documentation.py
+++ scripts/test_documentation.py
@@ -1,5 +1,6 @@
 import os
 import re
+import sys
 from enum import Enum
 
 MULTIPLE_DESCRIPTIONS = 'TDMD'
@@ -178,6 +179,24 @@ for filename in os.listdir("."):
                 current_obj.add_code_line(line)
 
 if __name__ == "__main__":
+    if "--file" in sys.argv:
+        file = sys.argv[sys.argv.index("--file") + 1]
+        files = [file]
+
+    if "--severity" in sys.argv:
+        severity = sys.argv[sys.argv.index("--severity") + 1]
+        for t in checks_by_item_type:
+            checks_by_item_type[t] = [c for c in checks_by_item_type[t] if str(c.severity).endswith(severity)]
+
+    if "--check" in sys.argv:
+        check = sys.argv[sys.argv.index("--check") + 1]
+        for t in checks_by_item_type:
+            checks_by_item_type[t] = [c for c in checks_by_item_type[t] if c.cid == check]
+
+    hidesuccesses = False
+    if "--hidesuccesses" in sys.argv:
+        hidesuccesses = True
+
     successes = {}
     failures = {}
 
@@ -189,7 +208,8 @@ if __name__ == "__main__":
         print(f"{obj.file} : {obj.obj_type} {obj.name}:")
         for result in obj.checks():
             if result.success:
-                print(f"OK: {result.message}")
+                if not hidesuccesses:
+                    print(f"OK: {result.message}")
                 successes[obj.file] += 1
             else:
                 print(f"ERROR: Check failed: <{result.message}>")
